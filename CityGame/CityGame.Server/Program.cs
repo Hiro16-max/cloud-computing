c15768042648e14afc3506d406cd93df6a660e63
Sergey <57636632+Hiro16-max@users.noreply.github.com>
2025-04-18 16:34:00 +0500

    Add files via upload

--- CityGame/CityGame.Server/Program.cs
+++ CityGame/CityGame.Server/Program.cs
@@ -1,11 +1,20 @@
 ﻿using CityGame.Server.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddGrpc();
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddGrpc();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-app.MapGrpcService<CityGameService>();
-app.MapGet("/", () => "gRPC сервер CityGame работает...");
+        app.MapGrpcService<CityGameService>();
+        app.MapGet("/", () => "gRPC сервер CityGame работает...");
 
-app.Run();
+        app.Run();
+    }
+}
